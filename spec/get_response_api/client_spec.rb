ba976380cf3bcacb5bb81947617dcc8f83b5cbe9
Rafael S <rafael.costa@klickpages.com.br>
2017-09-14 16:30:02 -0300

    Add campaigns method to client and refactors connection requests.

--- spec/get_response_api/client_spec.rb
+++ spec/get_response_api/client_spec.rb
@@ -2,43 +2,85 @@ require 'spec_helper'
 require 'webmock/rspec'
 
 RSpec.describe GetResponseApi::Client do
-  let(:header) { {'Content-Type': 'application/json'} }
+  let(:header) { {'Content-Type' => 'application/json'} }
   let(:url)  { 'https://api.getresponse.com/v3' }
   let(:client) { described_class.new('1236547hjuh') }
+
   describe '#account' do
     before do
       WebMock.stub_request(:get, "#{url}/accounts")
-        .to_return(body: response, headers: header)
+             .to_return(body: response, headers: header)
     end
 
     context 'when the request is valid' do
-      let(:response) { result.to_json }
       let(:result) do
         {
-          "accountId"=>"pdIhh",
-         "firstName"=>"Jane",
-         "lastName"=>"Doe",
-         "email"=>"jane.doe@test.com"
-       }
+          'accountId' => 'pdIhh',
+          'firstName' => 'Jane',
+          'lastName' => 'Doe',
+          'email' => 'jane.doe@test.com'
+        }
       end
+      let(:response) { result.to_json }
 
-      subject{ client.account() }
+      subject { client.account }
 
       it { is_expected.to eq(result) }
     end
 
     context 'when the request is invalid' do
       let(:error_message) do
-        'Unable to authenticate request. Check credentials or authentication method details'
+        'Unable to authenticate request. ' \
+        'Check credentials or authentication method details'
       end
       let(:response) do
         {
-          "httpStatus"=>401,
-          "message"=>error_message
+          'httpStatus' => 401,
+          'message' => error_message
         }.to_json
       end
 
-      subject{ client.account() }
+      subject { client.account }
+
+      it { is_expected.to eq(error_message) }
+    end
+  end
+
+  describe '#campaigns' do
+    before do
+      WebMock.stub_request(:get, "#{url}/campaigns")
+             .to_return(body: response, headers: header)
+    end
+
+    context 'when the request is valid' do
+      let(:result) do
+        [{
+          "campaignId" => "B",
+          "name" => "TestCampaigns",
+          "description" => "New test campaign",
+          "isDefault" => "true",
+        }]
+      end
+      let(:response) { result.to_json }
+
+      subject { client.campaigns }
+
+      it { is_expected.to eq(result) }
+    end
+
+    context 'when the request is invalid' do
+      let(:error_message) do
+        'Unable to authenticate request. ' \
+        'Check credentials or authentication method details'
+      end
+      let(:response) do
+        {
+          'httpStatus' => 401,
+          'message' => error_message
+        }.to_json
+      end
+
+      subject { client.campaigns }
 
       it { is_expected.to eq(error_message) }
     end
